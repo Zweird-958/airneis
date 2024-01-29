47ea0c28815535f6813f69a6ecb8ce7de80ca988
HAMEL Julien <zweir.dd@gmail.com>
2024-01-29 11:40:09 +0100

    fix: can't load new classes on /components folder

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,7 +3,10 @@ import type { Config } from "tailwindcss"
 import baseConfig from "../../tailwind.config"
 
 const config: Config = {
-  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {},
   plugins: [],
   presets: [baseConfig],
