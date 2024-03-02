88b024d6541e4da77801db2be68afa5502926872
Léon <leon.cariou@cyna-it.fr>
2024-03-02 13:39:24 +0100

    feat(web): add api route create user

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,12 @@
 import categoriesRouter from "./router/categories"
 import productsRouter from "./router/products"
+import usersRouter from "./router/users"
 import { createTRPCRouter } from "./trpc"
 
 export const appRouter = createTRPCRouter({
   products: productsRouter,
   categories: categoriesRouter,
+  users: usersRouter,
 })
 
 export type AppRouter = typeof appRouter
