61ffc34c8d0aa684d4e295f0062904957c14c54d
Vitor Sousa <reydux95@gmail.com>
2024-03-03 15:50:57 +0100

    refactor(tailwind): move colors to root config

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -4,27 +4,7 @@ import baseConfig from "../../tailwind.config"
 
 const config: Config = {
   content: ["./src/app/**/*.tsx", "./src/components/**/*.tsx"],
-  theme: {
-    extend: {
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        error: "hsl(var(--error))",
-        card: "hsl(var(--card))",
-      },
-      borderColor: {
-        default: "hsl(var(--border))",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
   presets: [baseConfig],
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,23 @@ const config: Config = {
       borderRadius: {
         default: "0.5rem",
       },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        error: "hsl(var(--error))",
+        card: "hsl(var(--card))",
+      },
+      borderColor: {
+        default: "hsl(var(--border))",
+      },
     },
   },
 }
