d83a5bb05e22d121bf5942e56e62a4bc9d54cf5c
HAMEL Julien <zweir.dd@gmail.com>
2024-03-04 21:36:49 +0100

    refactor(sign-in): add px-4

--- apps/web/src/app/[locale]/sign-in/page.tsx
+++ apps/web/src/app/[locale]/sign-in/page.tsx
@@ -21,7 +21,7 @@ const SignIn = async ({ params: { locale } }: PageProps) => {
   const { forms } = await getTranslations(locale)
 
   return (
-    <div className="h-screen flex items-center justify-center w-full absolute top-0">
+    <div className="h-screen flex items-center justify-center w-full absolute top-0 px-4">
       <div className="max-w-96 mx-auto rounded-default p-4 bg-card w-full">
         <h1 className="text-center text-xl text-primary font-semibold">
           {forms.signIn}
