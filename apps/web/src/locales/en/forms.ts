2edf3e7e911d26af5be96fc0cac8f9af8a549fd0
Léon <leon.cariou@cyna-it.fr>
2024-03-04 20:42:12 +0100

    Add SignUpForm component and update form translations

--- apps/web/src/locales/en/forms.ts
+++ apps/web/src/locales/en/forms.ts
@@ -3,6 +3,11 @@ export default {
   username: "Username",
   name: "Name",
   description: "Description",
+  email: "Email",
+  password: "Password",
+  firstName: "First name",
+  lastName: "Last name",
+  signUp: "Sign up",
   image: {
     label: "Image",
     description: "Image URL",
