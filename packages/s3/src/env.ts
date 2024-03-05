4c6ffe68647a4c32412e2b67cce29ca849862ea0
rdx <reydux95@gmail.com>
2024-03-05 15:15:15 +0100

    chore(s3): add S3_PORT env

--- packages/s3/src/env.ts
+++ packages/s3/src/env.ts
@@ -4,12 +4,14 @@ import { z } from "zod"
 dotenv({ path: "../../.env" })
 
 const schema = z.object({
+  S3_PORT: z.coerce.number(),
   S3_URL: z.string().url(),
   S3_BUCKET: z.string(),
-  S3_ACCESS_KEY_ID: z.string(),
-  S3_SECRET_KEY: z.string(),
+  S3_ACCESS_KEY_ID: z.string().default(""),
+  S3_SECRET_KEY: z.string().default(""),
 })
 const env = schema.parse({
+  S3_PORT: process.env.S3_PORT,
   S3_URL: process.env.S3_URL,
   S3_BUCKET: process.env.S3_BUCKET,
   S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY,
