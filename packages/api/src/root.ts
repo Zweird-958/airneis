d2b95969b86da9a0990db356b94bc91dca60502e
Léon <leon.cariou@cyna-it.fr>
2024-03-05 10:28:41 +0100

    Fix merge conflicts

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -5,10 +5,7 @@ import { createTRPCRouter } from "./trpc"
 
 export const appRouter = createTRPCRouter({
   products: productsRouter,
-<<<<<<< HEAD
-=======
   categories: categoriesRouter,
->>>>>>> 024d1441d165ad40a3e35db80a68550316fcb1ee
   users: usersRouter,
 })
 
