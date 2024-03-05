ccfbf888e366e7a148802f687f661926c9780337
Léon <leon.cariou@cyna-it.fr>
2024-03-05 12:26:55 +0100

    destructuration code input in user route

--- packages/api/src/router/users.ts
+++ packages/api/src/router/users.ts
@@ -8,25 +8,27 @@ import { createTRPCRouter, publicProcedure } from "../trpc"
 const usersRouter = createTRPCRouter({
   create: publicProcedure
     .input(signUpSchema)
-    .mutation(async ({ ctx, input }) => {
-      const user = await ctx.entities.user.findOne({ email: input.email })
+    .mutation(
+      async ({ ctx, input: { email, firstName, lastName, password } }) => {
+        const user = await ctx.entities.user.findOne({ email })
 
-      if (user) {
-        return false
-      }
+        if (user) {
+          return false
+        }
 
-      const hashedPassword = await hash(input.password, env.HASH_SALT_COUNT)
+        const hashedPassword = await hash(password, env.HASH_SALT_COUNT)
 
-      ctx.entities.user.create({
-        email: input.email,
-        firstName: input.firstName,
-        lastName: input.lastName,
-        password: hashedPassword,
-      })
-      await ctx.em.flush()
+        ctx.entities.user.create({
+          email,
+          firstName,
+          lastName,
+          password: hashedPassword,
+        })
+        await ctx.em.flush()
 
-      return true
-    }),
+        return true
+      },
+    ),
 })
 
 export default usersRouter
