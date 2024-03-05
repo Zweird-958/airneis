b01afe9c3994454dd69ae24cb66f37f1e2b0d0b4
Léon <leon.cariou@cyna-it.fr>
2024-03-05 17:43:35 +0100

    Add password validation regex to signUpSchema

--- packages/schemas/src/users.ts
+++ packages/schemas/src/users.ts
@@ -4,7 +4,10 @@ export const signUpSchema = z.object({
   firstName: z.string(),
   lastName: z.string(),
   email: z.string().email(),
-  password: z.string().min(8),
+  password: z
+    .string()
+    .min(8)
+    .regex(/(?=.*\p{Lu})(?=.*\p{Ll})(?=.*\d)(?=.*[^\d\p{L}]).*/u),
 })
 
 export type SignUpInput = z.infer<typeof signUpSchema>
