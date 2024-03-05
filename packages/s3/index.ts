ad282946a31adbc32aaa52ae3f776e4b37f66d60
rdx <reydux95@gmail.com>
2024-03-05 16:21:46 +0100

    refactor(s3): move fn

--- packages/s3/index.ts
+++ packages/s3/index.ts
@@ -1,14 +1,2 @@
-import { S3Client } from "@aws-sdk/client-s3"
-
-import env from "./src/env"
-
-export const s3 = new S3Client({
-  region: "auto",
-  endpoint: env.S3_URL,
-  credentials: {
-    accessKeyId: env.S3_ACCESS_KEY_ID,
-    secretAccessKey: env.S3_SECRET_KEY,
-  },
-})
-
 export * from "@aws-sdk/client-s3"
+export * from "./src/client"
