8ea5d7d74a8c9b09964c611ddec4778e6a2d121b
Léon <leon.cariou@cyna-it.fr>
2024-03-06 21:00:15 +0100

    Refactor sign-in and sign-up pages to use FormLayout component

--- apps/web/src/app/[locale]/sign-up/page.tsx
+++ apps/web/src/app/[locale]/sign-up/page.tsx
@@ -1,20 +1,15 @@
-import { PageProps } from "@airneis/types"
-
 import SignUpForm from "@/components/forms/SignUpForm"
+import { FormLayout } from "@/components/ui/Form/FormLayout"
+import { PageProps } from "@/types/common"
 import getTranslations from "@/utils/locale/getTranslations"
 
 const SignUp = async ({ params: { locale } }: PageProps) => {
   const { forms } = await getTranslations(locale)
 
   return (
-    <div className="h-screen flex items-center justify-center w-full absolute top-0">
-      <div className="max-w-96 mx-auto rounded-default p-4 bg-card w-full">
-        <h1 className="text-center text-xl text-primary font-semibold">
-          {forms.signUp}
-        </h1>
-        <SignUpForm />
-      </div>
-    </div>
+    <FormLayout labelForm={forms.signUp}>
+      <SignUpForm />
+    </FormLayout>
   )
 }
 
