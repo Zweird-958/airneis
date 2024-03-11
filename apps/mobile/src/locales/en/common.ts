d798e9deedd23bc3160d0a3c264d3e0fe48780d6
Léon <leon.cariou@cyna-it.fr>
2024-03-11 14:49:12 +0100

    Add sign-in functionality to language files

--- apps/mobile/src/locales/en/common.ts
+++ apps/mobile/src/locales/en/common.ts
@@ -1,4 +1,7 @@
 export default {
   changeLanguage: "Change language to {language}",
   home: "Home",
+  signIn: "Sign in",
+  password: "Password",
+  email: "Email",
 } as const
--- apps/mobile/src/locales/fr/common.ts
+++ apps/mobile/src/locales/fr/common.ts
@@ -1,4 +1,7 @@
 export default {
   changeLanguage: "Mettre la langue à {language}",
   home: "Accueil",
+  signIn: "Se connecter",
+  password: "Mot de passe",
+  email: "Email",
 } as const
