f4eaa5f3ede192c6b7180ee433251e1691d71bdb
Léon <leon.cariou@cyna-it.fr>
2024-03-14 11:03:37 +0100

    add emailSchema and add in signup et signin schema

--- packages/schemas/src/users.ts
+++ packages/schemas/src/users.ts
@@ -1,13 +1,15 @@
 import { z } from "zod"
 
+export const emailSchema = z.string().email()
+
 export const signInSchema = z.object({
-  email: z.string().email(),
+  email: emailSchema,
   password: z.string().min(8),
 })
 export const signUpSchema = z.object({
   firstName: z.string(),
   lastName: z.string(),
-  email: z.string().email(),
+  email: emailSchema,
   password: z
     .string()
     .min(8)
