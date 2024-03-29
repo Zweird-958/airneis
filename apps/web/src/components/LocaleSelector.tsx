27cf707c198dc689b5b324f55afbabe79917b82b
HAMEL Julien <zweir.dd@gmail.com>
2024-03-29 15:41:58 +0100

    fix(i18n): bad redirection when you change language

--- apps/web/src/components/LocaleSelector.tsx
+++ apps/web/src/components/LocaleSelector.tsx
@@ -1,20 +1,22 @@
 "use client"
 
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
 import { sharedConfig } from "@airneis/config"
 
-import Link from "@/components/ui/Link"
 import { useTranslation } from "@/i18n/client"
 
 const LocaleSelector = () => {
   const { locale } = useTranslation()
+  const pathname = usePathname()
 
   return (
     <div className="flex gap-1">
       {sharedConfig.languageKeys.map((language: string, index: number) => (
         <div key={language} className="flex gap-1">
           <Link
-            data-locale={language}
-            href={`/${language}`}
+            href={pathname.replace(`/${locale}`, `/${language}`)}
             className={locale === language ? "underline" : ""}
           >
             {language.toUpperCase()}
