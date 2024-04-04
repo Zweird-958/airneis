63bb0366b59c740acee29abf63ae7a66bae83225
HAMEL Julien <zweir.dd@gmail.com>
2024-04-04 22:15:32 +0200

    fix(image): add lang in context

--- apps/web/src/app/api/image/route.ts
+++ apps/web/src/app/api/image/route.ts
@@ -1,12 +1,13 @@
 import { NextRequest, NextResponse } from "next/server"
 
 import { createCaller, createTRPCContext } from "@airneis/api"
+import { sharedConfig } from "@airneis/config"
 
 export const POST = async (req: NextRequest) => {
   const formData = await req.formData()
   const file = formData.get("file") as File
   const buffer = Buffer.from(await file.arrayBuffer())
-  const caller = createCaller(createTRPCContext())
+  const caller = createCaller(createTRPCContext(sharedConfig.fallbackLng))
   const imageUrl = await caller.images.create({
     buffer,
     type: file.type,
