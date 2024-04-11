5ec3d0d777f902e114d4f9edf754ac450889b1ec
Léon <leon.cariou@cyna-it.fr>
2024-04-11 12:08:38 +0200

    Add materialsRouter to API router and createMaterialSchema to schemas

---
+++ packages/api/src/router/materials.ts
@@ -0,0 +1,28 @@
+import { TRPCError } from "@trpc/server"
+import { createTRPCRouter, publicProcedure } from "packages/api/src/trpc"
+import { Locale } from "packages/config"
+
+import { createMaterialSchema } from "@airneis/schemas"
+
+const materialsRouter = createTRPCRouter({
+  create: publicProcedure
+    .input(createMaterialSchema)
+    .mutation(async ({ ctx, input: { name } }) => {
+      const materialExists = await ctx.entities.material.findOne({
+        name,
+      })
+
+      if (materialExists) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "Material already exists",
+        })
+      }
+
+      ctx.entities.material.create({
+        name: name as Record<Locale, string>,
+      })
+    }),
+})
+
+export default materialsRouter
