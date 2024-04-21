b56c253d84f5d45d02103be9703b809181e0528a
rdx <reydux95@gmail.com>
2024-04-21 03:24:12 +0200

    feat(stripe): add client-side & server-side clients

--- packages/stripe/src/utils/client.ts
+++ packages/stripe/src/utils/client.ts
@@ -1,10 +1,12 @@
+"use client"
+
 import { Stripe } from "@stripe/stripe-js"
 import { loadStripe } from "@stripe/stripe-js/pure"
 
 import env from "../env"
 
 /**
- * Recommended pattern for initializing Stripe.js.
+ * Recommended pattern for initializing Stripe.js
  *
  * See: https://vercel.com/guides/getting-started-with-nextjs-typescript-stripe
  */
@@ -12,7 +14,7 @@ import env from "../env"
 // eslint-disable-next-line init-declarations
 let stripePromise: Promise<Stripe | null>
 export const getStripe = () => {
-  stripePromise ||= loadStripe(env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
+  stripePromise ||= loadStripe(env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 
   return stripePromise
 }
