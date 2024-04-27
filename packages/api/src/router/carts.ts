e5412133722d0654e6835a84168dc57e757c6502
HAMEL Julien <zweir.dd@gmail.com>
2024-04-27 16:22:52 +0200

    feat(cart): save locale cart to db on sign in

--- packages/api/src/router/carts.ts
+++ packages/api/src/router/carts.ts
@@ -1,5 +1,5 @@
-import { addToCartSchema } from "@airneis/schemas"
-import { Product } from "@airneis/types"
+import { addToCartSchema, cartSchema } from "@airneis/schemas"
+import { Id, Product } from "@airneis/types"
 
 import { authedProcedure, createTRPCRouter } from "../trpc"
 
@@ -45,6 +45,57 @@ const cartsRouter = createTRPCRouter({
       quantity,
     }))
   }),
+  saveLocale: authedProcedure
+    .input(cartSchema)
+    .mutation(async ({ ctx: { entities, em, session }, input: localeCart }) => {
+      const user = await entities.user.findOneOrFail({
+        id: session.user.id,
+      })
+      const products = await entities.product.find({
+        id: {
+          $in: localeCart.map(({ id }) => id as Id),
+        },
+      })
+      const productsInCart = await entities.cart.find({
+        user,
+        product: {
+          $in: products,
+        },
+      })
+      const productNotInCart = products.filter((product) =>
+        productsInCart.find(
+          ({ product: cartProduct }) => cartProduct.id === product.id,
+        ),
+      )
+
+      for (const product of productsInCart) {
+        const localeProduct = localeCart.find(
+          ({ id }) => id === product.product.id,
+        )
+
+        if (!localeProduct) {
+          continue
+        }
+
+        product.quantity = localeProduct.quantity
+      }
+
+      for (const product of productNotInCart) {
+        const localeProduct = localeCart.find(({ id }) => id === product.id)
+
+        if (!localeProduct) {
+          continue
+        }
+
+        entities.cart.create({
+          product,
+          quantity: localeProduct.quantity,
+          user,
+        })
+      }
+
+      await em.flush()
+    }),
 })
 
 export default cartsRouter
