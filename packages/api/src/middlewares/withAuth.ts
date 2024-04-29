9e5ddcb04b671bc5f3d9786a665062276bebe963
HAMEL Julien <zweir.dd@gmail.com>
2024-04-29 11:31:58 +0200

    styles: remove useless space

---
+++ packages/api/src/middlewares/withAuth.ts
@@ -0,0 +1,23 @@
+import { TRPCError } from "@trpc/server"
+
+import { UserRoles } from "@airneis/types"
+
+import { tRPCInit } from "../trpc"
+import getSession from "../utils/getSession"
+
+const withAuth = (t: tRPCInit, minimumRole: keyof typeof UserRoles) =>
+  t.middleware(({ ctx, next }) => {
+    const session = getSession()
+
+    if (!session) {
+      throw new TRPCError({ code: "UNAUTHORIZED" })
+    }
+
+    if (minimumRole === "ADMIN" && session.user.role !== "ADMIN") {
+      throw new TRPCError({ code: "FORBIDDEN" })
+    }
+
+    return next({ ctx: { ...ctx, session } })
+  })
+
+export default withAuth
