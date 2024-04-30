721c50de8e45aa3f26f2660e88aec6b7ecab87a3
rdx <reydux95@gmail.com>
2024-04-30 11:37:44 +0200

    fix(checkout): add locale to urls

--- packages/api/src/router/checkout.ts
+++ packages/api/src/router/checkout.ts
@@ -1,43 +1,46 @@
 import { TRPCError } from "@trpc/server"
 
+import config from "../config"
 import { authedProcedure, createTRPCRouter } from "../trpc"
 
 const checkoutRouter = createTRPCRouter({
-  createSession: authedProcedure.mutation(async ({ ctx: { req, stripe } }) => {
-    const origin = req.headers.get("origin")
+  createSession: authedProcedure.mutation(
+    async ({ ctx: { req, stripe, lang } }) => {
+      const origin = req.headers.get("origin")
 
-    if (!origin) {
-      throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Origin header is missing",
-      })
-    }
+      if (!origin) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Origin header is missing",
+        })
+      }
 
-    const checkoutSession = await stripe.checkout.sessions.create({
-      mode: "payment",
-      line_items: [
-        {
-          quantity: 2,
-          price_data: {
-            currency: "eur",
-            product_data: { name: "Table" },
-            unit_amount: 2000,
+      const checkoutSession = await stripe.checkout.sessions.create({
+        mode: "payment",
+        line_items: [
+          {
+            quantity: 2,
+            price_data: {
+              currency: config.currency.toLowerCase(),
+              product_data: { name: "Table" },
+              unit_amount: 2000,
+            },
           },
-        },
-      ],
-      success_url: `${origin}/result?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${origin}/result?session_id={CHECKOUT_SESSION_ID}`,
-    })
-
-    if (!checkoutSession.url) {
-      throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Failed to create checkout session",
+        ],
+        success_url: `${origin}/${lang}/result?sessionId={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${origin}/${lang}/result?sessionId={CHECKOUT_SESSION_ID}`,
       })
-    }
 
-    return checkoutSession.url
-  }),
+      if (!checkoutSession.url) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to create checkout session",
+        })
+      }
+
+      return checkoutSession.url
+    },
+  ),
 })
 
 export default checkoutRouter
