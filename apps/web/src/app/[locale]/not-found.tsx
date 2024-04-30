307819cc157c198d9e83ec919a439bb5a5dc52da
rdx <reydux95@gmail.com>
2024-04-30 15:01:50 +0200

    feat(i18n): translate not-found page

--- apps/web/src/app/[locale]/not-found.tsx
+++ apps/web/src/app/[locale]/not-found.tsx
@@ -1,3 +1,25 @@
-const NotFound = () => <p>Page not found!</p>
+import { cookies } from "next/headers"
+
+import { Locale, sharedConfig } from "@airneis/config"
+
+import { useTranslation } from "@/i18n"
+
+const NotFound = async () => {
+  /**
+   * Next.js doesn't provide custom params (e.g. `locale`) in not-found pages.
+   * Becaues of that, we we need to get creative.
+   *
+   * Here we use the lang cookie set by the middleware, it should never be empty.
+   */
+  const { value: lang } = cookies().get(sharedConfig.localeCookieKey)!
+  const { t } = await useTranslation(lang as Locale, "common")
+
+  return (
+    <div>
+      <h1>{t("notFound.title")}</h1>
+      <p>{t("notFound.description")}</p>
+    </div>
+  )
+}
 
 export default NotFound
