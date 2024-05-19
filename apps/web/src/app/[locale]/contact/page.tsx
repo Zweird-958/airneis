9a1ea2d6db06e5f3c86ce54161852fe46bf46507
Léon <leon.cariou@cyna-it.fr>
2024-05-19 19:12:52 +0200

    feat: Add ContactPage with ContactForm component

--- apps/web/src/app/[locale]/contact/page.tsx
+++ apps/web/src/app/[locale]/contact/page.tsx
@@ -1,2 +1,16 @@
-const ContactPage = () => {
-    
+import ContactForm from "@/components/forms/ContactForm"
+import { FormLayout } from "@/components/ui/Form/FormLayout"
+import { useTranslation } from "@/i18n"
+import { PageProps } from "@/types/common"
+
+const ContactPage = async ({ params: { locale } }: PageProps) => {
+  const { t } = await useTranslation(locale, "forms")
+
+  return (
+    <FormLayout labelForm={t("contact.title")}>
+      <ContactForm />
+    </FormLayout>
+  )
+}
+
+export default ContactPage
--- apps/web/src/components/forms/ContactForm.tsx
+++ apps/web/src/components/forms/ContactForm.tsx
@@ -1,7 +1,10 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
 import { SubmitHandler, useForm } from "react-hook-form"
 import { toast } from "sonner"
 
-import { CreateMessageInput } from "@airneis/schemas"
+import { CreateMessageInput, createMessageSchema } from "@airneis/schemas"
 
 import DescriptionField from "@/components/forms/fields/DescriptionField"
 import EmailField from "@/components/forms/fields/EmailField"
@@ -13,24 +16,26 @@ import { useTranslation } from "@/i18n/client"
 import api from "@/trpc/client"
 
 const ContactForm = () => {
-  const form = useForm({
+  const form = useForm<CreateMessageInput>({
+    resolver: zodResolver(createMessageSchema),
     defaultValues: {
       email: "",
       subject: "",
       description: "",
     },
   })
+  const { t } = useTranslation("forms")
   const { onError } = useErrorHandler()
   const { mutate } = api.contact.create.useMutation({
     onError,
     onSuccess: () => {
       toast.success(t("contact.success"), { duration: 5000 })
+      form.reset()
     },
   })
   const onSubmit: SubmitHandler<CreateMessageInput> = (values) => {
     mutate(values)
   }
-  const { t } = useTranslation("forms")
 
   return (
     <Form ctx={form} onSubmit={onSubmit} className="space-y-6">
